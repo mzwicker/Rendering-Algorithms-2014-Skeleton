3fca71b012564ee6930d9412b8cf65e0e3e9cbb7
Matthias Zwicker <zwicker@iam.unibe.ch>
2014-02-06 12:35:37 +0100

    package info testscenes

--- rt/src/rt/testscenes/package-info.java
+++ rt/src/rt/testscenes/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Simple test scenes to compare your renderer to a reference solution.
  * Output images of these scenes are in the "testscenes output" directory. 
+ * These scenes cannot be rendered with the base code because they use
+ * additional classes that you are supposed to implement!
  */
 package rt.testscenes;
