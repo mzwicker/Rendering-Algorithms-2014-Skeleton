23128074a9a22d2e32d4342c91255096fb9731f7
panmari <smmuzi@gmail.com>
2014-06-19 14:01:00 +0200

    StaticVecmath now working for arbitrary tuples

--- rt/src/rt/StaticVecmath.java
+++ rt/src/rt/StaticVecmath.java
@@ -4,14 +4,14 @@ import javax.vecmath.*;
 
 public class StaticVecmath {
 
-	public static float dist2(Vector3f v1, Vector3f v2)
+	public static float dist2(Tuple3f v1, Tuple3f v2)
 	{
 		Vector3f tmp = new Vector3f(v1);
 		tmp.sub(v2);
 		return tmp.lengthSquared();
 	}
 	
-	public static Vector3f sub(Vector3f v1, Vector3f v2)
+	public static Vector3f sub(Tuple3f v1, Tuple3f v2)
 	{
 		Vector3f r = new Vector3f(v1);
 		r.sub(v2);
